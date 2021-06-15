2dd95de6fd6787c630cebd411e18516ce16adac5
Tamara <tamara.fendez@gmail.com>
2021-06-15 18:58:13 -0400

    toggle

--- public/covid19/assets/scripts.js
+++ public/covid19/assets/scripts.js
@@ -175,3 +175,73 @@ const LlenarTabla = async (data, table) => {
     $(`#${table} tbody`).append(rows);
 }
 
+// para obtener el JWT
+$('#js-form').submit(async (event) => {
+    event.preventDefault();
+    const email = document.getElementById('txtEmail').value;  
+    const password = $('#txtPass').val();
+    const JWT = await postData(email,password);
+    //const album = await getImage(JWT);
+    toggleLogin();
+    console.log(JWT);
+    
+})
+
+const postData = async (email, password) => {
+    try {
+        const response = await fetch('http://localhost:3000/api/login', {
+            method:'POST',
+            body: JSON.stringify({
+                email: email, 
+                password: password
+            })
+        })
+        const {token} = await response.json()
+
+        localStorage.setItem('jwt-token', token)
+        return token
+    } catch (err) {
+        console.error(`Error: ${err}`);
+    }
+}
+
+const getImage = async (token) => {
+    try {
+        const response = await fetch('http://localhost:3000/api/albums', {
+            method:'GET',
+            headers: {
+                Authorization: `Bearer ${token} `
+            }
+        })
+        const album = await response.json()
+        return album.data
+    } catch (err) {
+        console.error(`Error: ${err}`);
+    }
+}
+
+// botones en login - logout
+
+const toggleLogin = () => {
+    
+    if($('#chile').hasClass('d-none')) {
+    $('#chile').removeClass('d-none');
+    } else {
+        $('#chile').addClass('d-none')
+        };
+    if($('#logout').hasClass('d-none')) {
+        $('#logout').removeClass('d-none');
+    } else {
+        $('#logout').addClass('d-none')
+        };
+    if($('#login').hasClass('d-none') == false) {
+        $('#login').addClass('d-none') 
+    } else {
+        $('#login').removeClass('d-none')
+        };
+
+}
+
+// $('aqui clases para identificar el elemento').addClass('d-none'); // agrega la clase d-none al elemento
+// $('aqui clases para identificar el elemento').removeClass('d-none'); // quita la clase d-none al elemento
+
